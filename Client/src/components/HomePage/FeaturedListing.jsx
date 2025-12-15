29fdfb989e5cb4979cc7dfaa967334e498be2759
saurabh kumar <saurabhkaswa04@gmail.com>
2025-12-15 11:08:10 +0530

    Feature/homepage : completed featured near you section with  basic listing card

--- Client/src/components/HomePage/FeaturedListing.jsx
+++ Client/src/components/HomePage/FeaturedListing.jsx
@@ -1,23 +1,30 @@
 import ListingCard from "../Listing/ListingCard"
-
+import home1 from "../../assets/images/home1.jpg"
+import home2 from "../../assets/images/home2.jpg"
+import home3 from "../../assets/images/home3.jpg"
+import home4 from "../../assets/images/home4.jpg"
+import home5 from "../../assets/images/home5.jpg"
 export default function FeaturedListings(){
     const listings = [
-        {_id:1,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: "../../assets/images/logo.png"},
-         {_id:1,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: "../../assets/images/logo.png"},
-         {_id:1,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: "../../assets/images/logo.png"},
-         {_id:1,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: "../../assets/images/logo.png"},
-         {_id:1,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: "../../assets/images/logo.png"},
-         {_id:1,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: "../../assets/images/logo.png"},
-         {_id:1,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: "../../assets/images/logo.png"}
-    
-    ]
+        {_id:1,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: home5},
+        {_id:5,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: home3},
+        {_id:4,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: home4},
+        {_id:6,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: home2},
+        {_id:2,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: home5},
+        {_id:7,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: home1},
+        {_id:3,title:"RK complex", price: 15000, location: "Delhi", type: "PG", image: home5},
+        ];
 
     return(
-        <div className="my-5">
+        <div className="my-5 mx-2">
             <h1 className="text-3xl font-bold  text-orange-400">
                 Featured Near You
             </h1>
-           
+            <div className="mx-2 my-2 flex gap-4  overflow-x-auto "> 
+           {listings.map((listing,idx)=>(
+            <ListingCard key={listing._id} listing={listing}/>
+           ))}
+           </div>
            
         </div>
     )
--- Client/src/components/Listing/ListingCard.jsx
+++ Client/src/components/Listing/ListingCard.jsx
@@ -1,11 +1,16 @@
 
-export default function ListingCard({key, listing}){
+export default function ListingCard({ listing}){
     return(
-        <div className="h-15 w-18">
-            <p>key</p>
-            <p>listing.title</p>
+        <div key={listing._id} className="border h-60 w-50 flex-shrink-0">
+            <div className="border h-33  bg-gradient-to-r from-orange-400 to-yellow-900">
+                <img src={listing.image} className="h-full w-full object-cover" alt="Home"/>
+            </div>
+           <div className="p-2 flex  flex-col ">
+            {/* <p>{listing._id}</p> */}
+            <p>{listing.title}</p>
         
-            <p>listing.price</p>
+            <p>Price: Rs. {listing.price}/month</p>
+            </div>
         </div>
     )
 }
