7f1bf8005c5421bf199d402d793b5d0324e43564
saurabh kumar <saurabhkaswa04@gmail.com>
2026-01-04 23:10:17 +0530

    started login and register page developing

--- Client/src/main.jsx
+++ Client/src/main.jsx
@@ -4,20 +4,23 @@ import { createBrowserRouter, createRoutesFromElements, Route, RouterProvider } 
 import './index.css'
 import App from './App.jsx'
 import Home from './pages/Home.jsx'
-import Register from './pages/Register.jsx'
 import Listing from './pages/Listing.jsx'
 import AddListing from './pages/AddListing.jsx'
 import ListingDetails from './pages/listingDetails.jsx'
+import Register from './pages/Register.jsx'
+import Login from './pages/Login.jsx'
 const routes = createBrowserRouter(
   createRoutesFromElements(
     <>
     <Route path="/" element={<App/>} >
+    
       <Route index element={<Home/>}/>
       <Route path="listing" element={<Listing/>}/>
       <Route path="/listing/add" element={<AddListing/>}/>
       <Route path="listing/:id" element={<ListingDetails/>}/>
     </Route>
     <Route path="/register" element={<Register/>}/>
+    <Route path="/login" element={<Login/>}/>
     </>
   )
 );
