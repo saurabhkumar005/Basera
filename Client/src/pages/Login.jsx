0695b22103f50e783d8ebc641427a4452130cdba
saurabh kumar <saurabhkaswa04@gmail.com>
2026-01-10 19:37:23 +0530

    updated add listing form UI and completed UI for login and register page

--- Client/src/pages/Login.jsx
+++ Client/src/pages/Login.jsx
@@ -1,4 +1,6 @@
 import loginBG from "../assets/images/loginBG.png"
+import OrangeButton from "../components/UI/orangeButton";
+import {Link} from 'react-router-dom'
 export default function Login() {
     const inputStyle =  "w-full p-3 rounded-2xl bg-gray-200 border-2 border-gray-200 focus:outline-none  hover:border-orange-500 focus:border-orange-500 placeholder-gray-500 shadow-[inset_2px_3px_6px_rgba(0,0,0,0.4)] focus:shadow-[inset_2px_0px_4px_rgba(0,0,0,0.6)]";
     return (
@@ -6,7 +8,7 @@ export default function Login() {
             <div
                 style={{ backgroundImage: `url(${loginBG})` }}
                 className={` w-screen h-screen bg-cover bg-center bg-no-repeat flex justify-center items-center`}>
-                <div className="p-6 w-96  bg-white/40 relative z-10 backdrop-blur-sm border border-white/90 shadow-xl rounded-3xl flex flex-col gap-6">
+                <div className="p-6 w-90 sm:w-120  bg-white/40 relative z-10 backdrop-blur-sm border border-white/90 shadow-xl rounded-4xl flex flex-col gap-2">
 
                     {/* Header */}
                     <div className="flex flex-col gap-1">
@@ -30,9 +32,11 @@ export default function Login() {
                             className={inputStyle}
                             placeholder="Enter your Password"
                         />
-                       <button className="bg-orange-500 hover:bg-orange-600 p-3 mt-4  rounded-3xl">Login</button>
+                    <OrangeButton placeholder={"Login"} type={"submit"}/>
                     </form>
-
+                    <div className=" flex justify-center">
+                    <Link to="/register" className=" hover:text-sky-700 hover:underline"> New User? Register Here</Link>
+                    </div>
                 </div>
             </div>
         </>
