450af6981c724f4d492f6195cdba91a84fdbe7f3
saurabh kumar <saurabhkaswa04@gmail.com>
2026-02-15 18:51:26 +0530

    Developed Edit Profile modal UI + Logic for the frontend part with image preview option + send data for updation to backend

--- Client/src/pages/UserProfile.jsx
+++ Client/src/pages/UserProfile.jsx
@@ -5,10 +5,12 @@ import profileBG from "../assets/images/profileBG.png"
 import dp1 from "../assets/images/anime.jpg"
 import { useState, useEffect } from "react";
 import ListingCard from "../components/Listing/ListingCard";
+import { UserRound, UserRoundPen, UserRoundPlus } from 'lucide-react';
+import EditProfile from "../modals/EditProfile";
 export default function UserProfile() {
     const { user, isAuthenticated } = useAuthContext();
     const [myListings, setMyListings] = useState([]);
-    
+    const [isEditModalOpen, setEditModal] = useState(false);
     useEffect(()=>{
         const fetchMyListings = async()=>{
             if(user){
@@ -36,11 +38,15 @@ export default function UserProfile() {
         <>
             {/* HEro part for profile page(upper part) */}
             <div style={{ backgroundImage: `url(${profileBG})` }}
-                className="w-full px-4 py-10  bg-cover relative bg-center bg-no-repeat flex justify-center items-center ">
+                className=" w-full px-4 py-10  bg-cover relative bg-center bg-no-repeat flex justify-center items-center ">
                 <div className=" sm:p-5  flex  justify-evenly gap-4 sm:gap-10" >
                     {/* Profile image and name section */}
-                    <img className="p-[0.2em] mt-3 bg-white w-44 h-44 lg:w-52 lg:h-52  rounded-full object-cover object-center"
-                        src={user.avatarUrl ? user.avatarUrl : dp1} />
+                    <div className=" bg-gray-700 flex justify-center items-center relative  mt-3  w-44 h-44 lg:w-52 lg:h-52  rounded-full ">
+                    {user.avatarUrl ? <img className="  w-44 h-44 lg:w-52 lg:h-52  rounded-full  object-cover object-center"
+                        src={user.avatarUrl} /> :
+                      <UserRoundPlus onClick={()=>setEditModal(true)} className=" hover:scale-104 pl-4 flex text-gray-400   w-3/6 h-3/6" />
+                    }
+                    </div>
                     <div className="   flex flex-col  gap-5 w-[60%] md:w-[60vw] ">
                         <div className=" text-white font-bold flex flex-col gap-1">
                             <h2 className="text-4xl font-bold ">{user.name}</h2>
@@ -55,7 +61,7 @@ export default function UserProfile() {
                             <button className="bg-white p-4 rounded-3xl">💬 Messages</button>
                         </div>
                         <div className="flex gap-5 p-1 ">
-                            <button className="bg-orange-600 px-4 py-2  text-white  rounded-md">Edit Profile</button>
+                            <button onClick={()=>{setEditModal(true)}} className="bg-orange-600 px-4 py-2  text-white  rounded-md">Edit Profile</button>
                             <button className="bg-orange-600 px-3 py-2 text-white   rounded-md">Change Password</button>
                           
                         </div>
@@ -91,6 +97,10 @@ export default function UserProfile() {
                 }
                 </div>
             </div>
+                
+               {isEditModalOpen && <EditProfile user={user} onClose={()=>setEditModal(false)}/>}
+          
+            
         </>
     )
 }
